49ee288ea594c04e5624d3caeae0a55966bddc1d
MathBatistela <42747473+MathBatistela@users.noreply.github.com>
2019-09-08 19:54:47 -0300

    Add files via upload

--- union.py
+++ union.py
@@ -29,11 +29,13 @@ for i in range(2):
     tm[i].tape_alphabet = lines[1].split()
     tm[i].whitespace = lines[2]
     tm[i].states = lines[3].split()
+    tm[i].states = [int(p) for p in tm[i].states]
+    tm[i].states.sort()
     tm[i].initial_state = lines[4]
     tm[i].final_states = lines[5].split()
     tm[i].number_of_tapes = lines[6]
     for j in range(7, len(lines)):
-        tm[i].transitions.append(lines[j])
+        tm[i].transitions.append(lines[j].split())
 
 
 def printTmData(tm_aux, i):
@@ -51,5 +53,84 @@ def printTmData(tm_aux, i):
     print()
 
 
-for i in range(2):
+# if tm[0].number_of_tapes != tm[1].number_of_tapes:
+tm.append(TuringMachine())
+
+# Função 2a linha
+for j in range(len(tm[0].input_alphabet)):
+    tm[2].input_alphabet.append(tm[0].input_alphabet[j])
+
+
+for i in range(len(tm[1].input_alphabet)):
+    if tm[1].input_alphabet[i] not in tm[0].input_alphabet:
+        tm[2].input_alphabet.append(tm[1].input_alphabet[i])
+#######################################################
+
+# Função 3a linha
+for j in range(len(tm[0].tape_alphabet)):
+    tm[2].tape_alphabet.append(tm[0].tape_alphabet[j])
+
+
+for i in range(len(tm[1].tape_alphabet)):
+    if tm[1].tape_alphabet[i] not in tm[0].tape_alphabet:
+        tm[2].tape_alphabet.append(tm[1].tape_alphabet[i])
+
+#######################################################
+
+# Função linha4
+letter = 65
+for i in range(26):
+    if chr(letter) not in tm[2].tape_alphabet:
+        tm[2].whitespace.append(chr(letter))
+        break
+    letter += 1
+
+#######################################################
+
+# Define estados
+tam_final = len(tm[0].states) + len(tm[1].states) + 1
+
+for i in range(tam_final):
+    tm[2].states.append(i)
+
+# linha 5
+
+# matriz_ref_states = [[], []]
+
+# states = 1
+# for i in range(2):
+#     var = len(tm[i].states)
+#     for j in range(var):
+#         matriz_ref_states[i].append(states)
+#         states += 1
+
+
+# matriz_ref_init = [[], []]
+
+# tm[2].initial_state.append(0)
+# for i in range(2):
+#     matriz_ref_init[i].append(tm[i].initial_state[0])
+
+
+# matriz_ref_final = [[], []]
+
+# for i in range(2):
+#     var2 = len(tm[i].final_states)
+#     for j in range(var2):
+#         matriz_ref_final[i].append(tm[i].final_states[j])
+
+# # for j in range(2):
+# #     for i in range(len(matriz_ref_final[0] + len(matriz_ref_final[1]))):
+# #         tm[2].final_states.append(matriz_ref_states[j][i])
+
+tm[2].initial_state.append('0')
+
+ref_tm1 = []
+
+
+for i in range(3):
     printTmData(tm[i], i)
+
+# print(matriz_ref_states)
+# print(matriz_ref_init)
+# print(matriz_ref_final)
